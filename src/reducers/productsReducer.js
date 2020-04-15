61970557c7fc1d6a8c508bf939a71a9c178a78d5
Fedeya <elfedeminaya@gmail.com>
2020-04-15 03:31:53 -0300

    [Feat] added reducers to add product types

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,3 +1,9 @@
+import { 
+  ADD_PRODUCT, 
+  ADD_PRODUCT_SUCCESS,
+  ADD_PRODUCT_ERROR
+} from '../types';
+
 const initialState = {
   products: [],
   error: null,
@@ -6,6 +12,23 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch(action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        loading: true
+      }
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: [...state.products, action.payload]
+      }
+    case ADD_PRODUCT_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      }
     default:
       return state;
   }
