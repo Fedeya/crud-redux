c4237a2793c796f4c7a5cebdd962e402a8444c9d
Fedeya <elfedeminaya@gmail.com>
2020-04-15 03:56:49 -0300

    [Feat] added get products from api and used the product component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getProductsAction } from '../actions/productActions';
+
+import Product from './Product';
 
 function Products() {
+
+  const dispatch = useDispatch();
+
+  const products = useSelector(state => state.products.products);
+  const error = useSelector(state => state.products.error);
+  const loading = useSelector(state => state.products.loading);
+
+  useEffect(() => {
+    const getProducts = () => dispatch(getProductsAction);
+    getProducts();
+  }, [])
+
   return (
     <Fragment>
       <h2 className="text-center my-5">
         List of Products
       </h2>
+      { error && <p className="font-weight-bold alert alert-danger text-center mt-4">There was an error</p> }
+      { loading && <p className="text-center">Loading...</p> }
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
           <tr>
@@ -15,7 +34,11 @@ function Products() {
           </tr>
         </thead>
         <tbody>
-          
+          {
+            products && products.map(product => (
+              <Product key={product.id} product={product} />
+            ))
+          }
         </tbody>
       </table>
     </Fragment>
