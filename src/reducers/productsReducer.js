d65f73f7a07e39d490cbef5120f2dc451777864d
Fedeya <elfedeminaya@gmail.com>
2020-04-15 03:50:10 -0300

    [Feat] reducers to get products

--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,7 +1,10 @@
 import { 
   ADD_PRODUCT, 
   ADD_PRODUCT_SUCCESS,
-  ADD_PRODUCT_ERROR
+  ADD_PRODUCT_ERROR,
+  GET_PRODUCTS,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR
 } from '../types';
 
 const initialState = {
@@ -12,6 +15,7 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch(action.type) {
+    case GET_PRODUCTS:
     case ADD_PRODUCT:
       return {
         ...state,
@@ -23,12 +27,21 @@ export default (state = initialState, action) => {
         loading: false,
         products: [...state.products, action.payload]
       }
+    case GET_PRODUCTS_ERROR:
     case ADD_PRODUCT_ERROR:
       return {
         ...state,
         loading: false,
         error: true
       }
+
+    case GET_PRODUCTS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        products: action.payload
+      }
+    }
     default:
       return state;
   }
