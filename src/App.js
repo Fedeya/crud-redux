f652fc2783117a33e82d975e53a048fada5fb240
Fedeya <elfedeminaya@gmail.com>
2020-04-15 00:38:12 -0300

    [Feat] used router with views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header';
+
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-    </Fragment>
+      <div className="container mt-4">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/new" component={NewProduct} />
+          <Route exact path="/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
