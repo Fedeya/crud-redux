9dff6bf8bafe41004a27973486297ab1a2d665fd
Fedeya <elfedeminaya@gmail.com>
2020-04-15 00:38:00 -0300

    [Feat] created views

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
       <div className="container">
-        <h1> Crud - React, Redux, REST API & Axios </h1>
+        <h1>
+          <Link to="/" className="text-light" >Crud - React, Redux, REST API & Axios</Link>
+        </h1>
       </div>
 
-      <a
-        href="/products/new"
+      <Link
+        to="/new"
         className="btn btn-danger new-post d-block d-md-inline-block"
       >
         Add Product &#43;
-      </a>
+      </Link>
     </nav>
   </header>
 );
---
+++ src/components/NewProduct.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+function NewProduct() {
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Add New Product
+            </h2>
+            <form>
+              <div className="form-group">
+                <label htmlFor="name">Product Name</label>
+                <input 
+                  type="text" 
+                  id="name"
+                  className="form-control"
+                  placeholder="Product Name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="price">Product Price</label>
+                <input 
+                  type="number" 
+                  id="price"
+                  className="form-control"
+                  placeholder="Product Price"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+                Add
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewProduct;
