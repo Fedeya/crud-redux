50d5d4b95199ee8e77f9f745a22f0659d7621134
Fedeya <elfedeminaya@gmail.com>
2020-04-15 01:51:05 -0300

    [Feat] used state and provider

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 
+// Views
+
 import Products from './components/Products';
 import NewProduct from './components/NewProduct';
 import EditProduct from './components/EditProduct';
 
+
+// Redux
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <div className="container mt-4">
-        <Switch>
-          <Route exact path="/" component={Products} />
-          <Route exact path="/new" component={NewProduct} />
-          <Route exact path="/edit/:id" component={EditProduct} />
-        </Switch>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <div className="container mt-4">
+          <Switch>
+            <Route exact path="/" component={Products} />
+            <Route exact path="/new" component={NewProduct} />
+            <Route exact path="/edit/:id" component={EditProduct} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
