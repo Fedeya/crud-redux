8a7ac84207fe56b5006af33e743f95d509e48758
Fedeya <elfedeminaya@gmail.com>
2020-04-16 17:05:17 -0300

    [Feat] added delete dispatch

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { deleteProductAction } from '../actions/productActions';
 
 function Product({ product }) {
+
+  const dispatch = useDispatch();
+  const deleteProduct = id => dispatch(deleteProductAction(id));
+
   return (
     <tr>
       <td className="font-weight-bold">{product.name}</td>
@@ -13,7 +20,10 @@ function Product({ product }) {
         >
           Edit
         </Link>
-        <button className="btn btn-danger">
+        <button 
+          className="btn btn-danger"
+          onClick={() => deleteProduct(product.id)}
+        >
           Delete
         </button>
       </td>
