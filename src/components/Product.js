18e1b92158a1ed3b698a809283cb00dea95334b5
Fedeya <elfedeminaya@gmail.com>
2020-04-16 21:25:36 -0300

    [Feat] added get product function in edit button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { deleteProductAction } from '../actions/productActions';
+import { deleteProductAction, getEditProductAction } from '../actions/productActions';
 
 function Product({ product }) {
 
   const dispatch = useDispatch();
+  const history = useHistory();
+
   const deleteProduct = id => dispatch(deleteProductAction(id));
+  const getEditProduct = p => dispatch(getEditProductAction(p));
+
+  const goToEditProduct = () => {
+    getEditProduct(product);
+    history.push(`/edit/${product.id}`);
+  }
 
   return (
     <tr>
       <td className="font-weight-bold">{product.name}</td>
       <td>{product.price}</td>
       <td className="actions">
-        <Link 
-          to={`/edit/${product.id}`}
+        <button
           className="btn btn-primary mr-2"
+          onClick={goToEditProduct}
         >
           Edit
-        </Link>
+        </button>
         <button 
           className="btn btn-danger"
           onClick={() => deleteProduct(product.id)}
